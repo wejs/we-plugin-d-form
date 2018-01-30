ff33cdd318a8d32c0b6f3677b1699133eb7e961a
albertosouza <alberto.souza.99@gmail.com>
2018-01-30 16:36:38 -0200

    add default form email response texts

--- plugin.js
+++ plugin.js
@@ -9,15 +9,39 @@ module.exports = function loadPlugin(projectPath, Plugin) {
   plugin.setConfigs({
     emailTypes: {
       formNewAnswerAlert: {
+        defaultSubject: '[{{siteName}}] Nova resposta no formulário {{formName}}',
+        defaultText: `O site {{siteName}} acaba de receber uma nova resposta no formulário {{formTitle}}.
+
+Url de visualização dos dados: {{answerUrl}}
+
+Enviada por:
+- Nome: {{name}}
+- Email {{email}}
+
+
+{{siteName}}
+{{siteUrl}}`,
+        defaultHTML: `<p>O site {{siteName}} acaba de receber uma nova resposta no formul&aacute;rio {{formTitle}}.</p>
+<p>Url de visualiza&ccedil;&atilde;o dos dados: {{answerUrl}}</p>
+<p>Enviada por:<br />- Nome: {{name}}<br />- Email {{email}}<br /><br /></p>
+<p>{{siteName}}<br />{{siteUrl}}</p>`,
         label: 'Email de aviso de nova responta em um dos formulários do sistema',
         templateVariables: {
+          formName: {
+            example: 'Form1',
+            description: 'Nome do formulário preenchido no editar formulário'
+          },
+          formTitle: {
+            example: 'Form 1',
+            description: 'Título do formulário'
+          },
           name: {
             example: 'Alberto Souza',
-            description: 'Nome da pessoa que respondeu o formulário'
+            description: 'Nome da pessoa que respondeu o formulário caso um campo com "name" nome exista no formulário'
           },
           email: {
             example: 'contact@linkysysytems.com',
-            description: 'Email da pessoa que respondeu o formulário'
+            description: 'Email da pessoa que respondeu o formulário caso um campo com "name" email exista no formulário'
           },
           answerUrl: {
             example: '/admin/#/d-form-answer/1',
